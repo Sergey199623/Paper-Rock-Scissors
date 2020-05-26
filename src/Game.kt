6429f6f098037b502d347a25ce362334a25e476a
Sergey <belyakovsergeyrus@yandex.ru>
2020-05-26 23:08:46 +0300

    last update 26.05.2020

--- src/Game.kt
+++ src/Game.kt
@@ -1,12 +1,12 @@
+import java.awt.Choice
 import java.util.*
 
 fun main(args: Array<String>) {
 
     val options = arrayOf("Paper", "Rock", "Scissors")
-
     val gameChoice = getGameChoice(options)
     val userChoice = getUserChoice(options)
-
+    printResult(userChoice, gameChoice)
 
 }
 
@@ -37,4 +37,20 @@ fun getUserChoice(optionsParam: Array<String>) : String {
         if (!isValidChoice) println("You must enter a valid choice.")
     }
     return userChoice
+}
+
+fun printResult(userChoice: String, gameChoice: String) {
+    val result: String
+
+    //Определяем результат
+
+    if(userChoice == gameChoice) result = "Tie!"
+    else if ((userChoice == "Rock" && gameChoice == "Scissors") ||
+        (userChoice == "Paper" && gameChoice == "Rock") ||
+        (userChoice == "Scissors" && gameChoice == "Paper")) result = "You win!"
+    else result = "You losw!"
+
+    //Выводим результат
+
+    println("You chose $userChoice. I chose $gameChoice. $result")
 }
